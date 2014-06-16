4898ec23b218f7f0919bf09997efac981d3ed331
ramprabhu73 <ram@ehealthaccess.com>
2014-06-16 16:24:22 +0530

    Comment on the codeset

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The file has 2 functions, the objective of the code is to make an inverse of a matrix
+## and return the solve() of it. If the inverse is already computed and in cache, it will
+## return the object in cache else it is recalculate it and output the value
 
 ## Write a short comment describing this function
 
